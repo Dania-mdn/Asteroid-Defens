b8c1d37cc8f8ef2d36d827e599a3521d66bd7b91
Dania-mdn <58430901+Dania-mdn@users.noreply.github.com>
2023-09-26 23:49:52 +0300

    asdas
    sss

--- AsteroidDefens/Assets/System/SystemEvent.cs
+++ AsteroidDefens/Assets/System/SystemEvent.cs
@@ -6,6 +6,9 @@ public class SystemEvent : MonoBehaviour
 {
     public static event Action Upgrade;
     public static event Action Step;
+    public static event Action EndStep;
+    public static event Action HitPlayer;
+    public static event Action EndGame;
     public static void DoUpgrade()
     {
         Upgrade?.Invoke();
@@ -13,5 +16,17 @@ public class SystemEvent : MonoBehaviour
     public static void DoStep()
     {
         Step?.Invoke();
+    }
+    public static void DoEndStep()
+    {
+        EndStep?.Invoke();
+    }
+    public static void DoHitPlayer()
+    {
+        HitPlayer?.Invoke();
+    }
+    public static void DoEndGame()
+    {
+        EndGame?.Invoke();
     }
 }
--- AsteroidDefens/Assets/System/UIManager.cs
+++ AsteroidDefens/Assets/System/UIManager.cs
@@ -1,20 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public Slider Health;
+    public int HealthCount = 5;
+    public TextMeshProUGUI Step;
     public int StepCount = 5;
+
+    public Match3Control Match3Control;
+
     private void OnEnable()
     {
         SystemEvent.Step += SetStep;
+        SystemEvent.HitPlayer += SetHealth;
     }
     private void OnDisable()
     {
         SystemEvent.Step -= SetStep;
+        SystemEvent.HitPlayer -= SetHealth;
+    }
+    private void Start()
+    {
+        Health.maxValue = HealthCount;
+        Health.value = HealthCount;
+        Step.text = StepCount.ToString();
     }
     private void SetStep()
     {
         StepCount--;
+        Step.text = StepCount.ToString();
+
+        if(StepCount == 0)
+        {
+            SystemEvent.DoEndStep();
+            Match3Control.enabled = false;
+        }
+    }
+    private void SetHealth()
+    {
+        HealthCount--;
+        Health.value = HealthCount;
+
+        if(HealthCount == 0)
+        {
+            SystemEvent.DoEndGame();
+        }
     }
 }
