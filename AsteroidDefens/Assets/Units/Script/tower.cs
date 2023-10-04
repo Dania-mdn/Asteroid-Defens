433712c1bdced446d9b98eba692968343ad94b13
Dania-mdn <58430901+Dania-mdn@users.noreply.github.com>
2023-10-04 23:13:37 +0300

    sdsdsd
    sss

--- AsteroidDefens/Assets/Units/Script/tower.cs
+++ AsteroidDefens/Assets/Units/Script/tower.cs
@@ -17,6 +17,10 @@ public class tower : MonoBehaviour
 
     public GameObject bulet;
 
+    public Transform[] firepont0;
+    private float angle;
+    private Vector2 dir;
+
     private void OnEnable()
     {
         SystemEvent.EndStep += SetIsSpawn;
@@ -40,6 +44,9 @@ public class tower : MonoBehaviour
 
         if(Asteroids.Count > 0)
         {
+            dir = Asteroids[0].transform.position - this.transform.position;
+            angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
             if (coldawn < 0)
             {
                 Fire(Asteroids[0]);
@@ -53,7 +60,8 @@ public class tower : MonoBehaviour
     }
     private void Fire(GameObject target)
     {
-        GameObject Bulet = Instantiate(bulet, transform.position, Quaternion.identity);
+        int j = Random.Range(0, 1);
+        GameObject Bulet = Instantiate(bulet, firepont0[j].position, Quaternion.identity);
         bulet buletScript = Bulet.GetComponent<bulet>();
         buletScript.target = target;
         buletScript.damage = parametrs.Damage[parametrs.LvL];
@@ -80,5 +88,6 @@ public class tower : MonoBehaviour
     private void SetIsSpawn(bool isSpawn)
     {
         IsSpawn = isSpawn;
+        //transform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
     }
 }
